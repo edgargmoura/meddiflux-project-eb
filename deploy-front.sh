0745d004f0ad3a719ae7a5d8939fe2b372db32af
Edgar <edgar.edps@gmail.com>
2024-11-14 12:21:04 -0300

    mudanças no docker-compose com variáveis de ambiente

---
+++ deploy-front.sh
@@ -0,0 +1,7 @@
+API_ENDPOINT=http://localhost:3001
+REACT_APP_API_URL=$API_ENDPOINT SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
+echo '>> Fazendo deploy dos assets'
+aws s3 sync client/build s3://meddiflux-assets-eb/hom --exclude "index.html" --profile tf-bia-lab
+
+echo '>> Fazendo deploy do index.html'
+aws s3 sync client/build s3://meddiflux-assets-eb/hom --exclude "*" --include "index.html" --profile tf-bia-lab
--- gerar-compose.sh
+++ gerar-compose.sh
@@ -1,4 +1,6 @@
 versao=$(git rev-parse HEAD | cut -c 1-7)
 echo TAG=$versao > .env
-docker compose -f docker-compose-eb.yml config > docker-compose-meddiflux.yml
+docker compose -f docker-compose-eb.yml config --no-normalize > docker-compose-meddiflux.yml
+sed -i '/^name:/d' docker-compose-meddiflux.yml
+sed -i '/server:$/a \    env_file: .env' docker-compose-meddiflux.yml
 mv docker-compose-meddiflux.yml docker-compose.yml
