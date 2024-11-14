9a43b008e660de05b533e22713ea7e1b8c614343
Edgar <edgar.edps@gmail.com>
2024-11-14 19:55:33 -0300

    mudando url do CDN

--- deploy-front.sh
+++ deploy-front.sh
@@ -1,5 +1,5 @@
 ./build.sh
-API_ENDPOINT=https://d380ey5zo6r1q8.cloudfront.net
+API_ENDPOINT=http://d380ey5zo6r1q8.cloudfront.net
 REACT_APP_API_URL=$API_ENDPOINT SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
 echo '>> Fazendo deploy dos assets'
 aws s3 sync client/build s3://meddiflux-assets-eb/hom --exclude "index.html" --profile tf-bia-lab
