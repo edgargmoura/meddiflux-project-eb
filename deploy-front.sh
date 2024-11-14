f819b3f7919e6c2d5fa0a6df262fee5100832ee4
Edgar <edgar.edps@gmail.com>
2024-11-14 14:25:42 -0300

    subindo nova versão do Dockerfile e arquivo deploy-front

--- deploy-front.sh
+++ deploy-front.sh
@@ -1,4 +1,4 @@
-API_ENDPOINT=http://localhost:3001
+API_ENDPOINT=http://meddiflux-hom.us-east-1.elasticbeanstalk.com
 REACT_APP_API_URL=$API_ENDPOINT SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
 echo '>> Fazendo deploy dos assets'
 aws s3 sync client/build s3://meddiflux-assets-eb/hom --exclude "index.html" --profile tf-bia-lab
