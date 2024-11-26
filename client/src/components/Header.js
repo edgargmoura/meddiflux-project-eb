271c6fac396a9a488567dacbe6f3ba4ad26377cc
Edgar <edgar.edps@gmail.com>
2024-11-26 18:00:20 -0300

    atualizando msg

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,7 +20,7 @@ const Header = ({ title, onAdd, showAdd }) => {
 };
 
 Header.defaultProps = {
-  title: "Ambiente de Produção da Meddiflux com Pipeline CDN e RDS V-5.5",
+  title: "Ambiente de Produção da Meddiflux com Pipeline CDN e RDS V-6",
 };
 
 export default Header;
